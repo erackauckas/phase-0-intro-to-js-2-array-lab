99b449ce149c05f807d0681318b295d2ad7976dd
Eric Rackauckas <erackauckas@gmail.com>
2022-04-07 14:12:55 -0700

    commit tree dfb67f58f635f507e3617c25539e700491a7b6da

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Write your solution here!
+
+var cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name){
+    cats.push(name);
+}
+
+function destructivelyPrependCat(name){
+    cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat(name){
+    cats.pop(name);
+}
+
+function destructivelyRemoveFirstCat(name){
+    cats.shift(name);
+}
+
+function appendCat(name){
+    var newcats = [...cats, name];
+    return newcats;
+} 
+
+function prependCat(name){
+    var newcats = [name, ...cats ];
+    return newcats;
+}
+
+function removeLastCat(){
+    var newcats = cats.slice(0,cats.length - 1)
+    return newcats;
+}
+
+function removeFirstCat(){
+    var newcats = cats.slice(1);
+    return newcats;
+}
